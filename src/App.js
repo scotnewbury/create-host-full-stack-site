7fe593f3c8363635eb746d8ff2a799471c950c6d
Scot Newbury <scot@scotnewbury.com>
2020-12-29 23:57:43 -0500

    Complete through 1.4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
-import HomePage from './pages/HomePage';
-
 import {
   BrowserRouter as Router,
   Route,
 } from 'react-router-dom';
 
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesList from './pages/ArticlesList';
+import ArticlePage from './pages/ArticlePage';
+
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path="/" component={HomePage} exact />
+        <div id="page-body">
+          <Route path="/" component={HomePage} exact />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/articles-list" component={ArticlesList} />
+          <Route path="/article" component={ArticlePage} />
+        </div>
       </div>
     </Router>
   );
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const AboutPage = () => (
+  <>
+    <h1>About Me</h1>
+
+  <p>Quisque velit nisi, pretium ut lacinia in, elementum id enim. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rutrum congue leo eget malesuada. Donec rutrum congue leo eget malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Nulla porttitor accumsan tincidunt.</p>
+
+  <p>Quisque velit nisi, pretium ut lacinia in, elementum id enim. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rutrum congue leo eget malesuada. Donec 
+    rutrum congue leo eget malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Nulla porttitor accumsan tincidunt.</p>
+
+  <p>Quisque velit nisi, pretium ut lacinia in, elementum id enim. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem ipsum dolor sit 
+    amet, consectetur adipiscing elit. Donec rutrum congue leo eget malesuada. Donec rutrum congue leo eget malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Nulla porttitor accumsan tincidunt.</p>
+  </>
+);
+
+export default AboutPage;
