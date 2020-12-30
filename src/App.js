a39b673877dc1ab40776856fab29fb59c3c1a5a1
Scot Newbury <scot@scotnewbury.com>
2020-12-29 23:40:47 -0500

    Complete through 1.3 plus page framework

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import HomePage from './pages/HomePage';
+
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Route path="/" component={HomePage} exact />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
