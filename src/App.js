9000f3db3f3a885a847f01d26b3af6cc3465a6f0
Scot Newbury <scot@scotnewbury.com>
2021-01-01 23:55:10 -0500

    Course complete through 1.5 - added navbar

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlesList from './pages/ArticlesList';
 import ArticlePage from './pages/ArticlePage';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <NavBar />
         <div id="page-body">
           <Route path="/" component={HomePage} exact />
           <Route path="/about" component={AboutPage} />
